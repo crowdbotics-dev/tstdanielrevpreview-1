55e4b94106757fcf19cbe4ef338a67a47636aeff
Crowdbotics <engineering@crowdbotics.com>
2021-08-06 13:44:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "tstdanielrevpreview_1Identifier",
+  url: "https://tstdanielrevpreview_1Identifier.botics.co",
+  api: "https://tstdanielrevpreview_1Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
